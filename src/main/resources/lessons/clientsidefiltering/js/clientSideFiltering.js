832ee8f0f83255e86f3a03c9a4cbdf52e0199aa7
xygenibot <xygeni_scan@xygeni.io>
2026-07-08 07:50:42 +0000

    Fix javascript.cross_site_scripting issue in src/main/resources/lessons/clientsidefiltering/js/clientSideFiltering.js

--- src/main/resources/lessons/clientsidefiltering/js/clientSideFiltering.js
+++ src/main/resources/lessons/clientsidefiltering/js/clientSideFiltering.js
@@ -15,27 +15,49 @@ function fetchUserData() {
 
 function ajaxFunction(userId) {
     $.get("clientSideFiltering/salaries?userId=" + userId, function (result, status) {
-        var html = "<table border = '1' width = '90%' align = 'center'";
-        html = html + '<tr>';
-        html = html + '<td>UserID</td>';
-        html = html + '<td>First Name</td>';
-        html = html + '<td>Last Name</td>';
-        html = html + '<td>SSN</td>';
-        html = html + '<td>Salary</td>';
+        var table = document.createElement("table");
+        table.setAttribute("border", "1");
+        table.setAttribute("width", "90%");
+        table.setAttribute("align", "center");
+
+        var headerRow = document.createElement("tr");
+        var headers = ["UserID", "First Name", "Last Name", "SSN", "Salary"];
+        for (var h = 0; h < headers.length; h++) {
+            var th = document.createElement("td");
+            th.textContent = headers[h];
+            headerRow.appendChild(th);
+        }
+        table.appendChild(headerRow);
 
         for (var i = 0; i < result.length; i++) {
-            html = html + '<tr id = "' + result[i].UserID + '"</tr>';
-            html = html + '<td>' + result[i].UserID + '</td>';
-            html = html + '<td>' + result[i].FirstName + '</td>';
-            html = html + '<td>' + result[i].LastName + '</td>';
-            html = html + '<td>' + result[i].SSN + '</td>';
-            html = html + '<td>' + result[i].Salary + '</td>';
-            html = html + '</tr>';
+            var row = document.createElement("tr");
+            row.id = String(result[i].UserID);
+
+            var userIdCell = document.createElement("td");
+            userIdCell.textContent = String(result[i].UserID);
+            row.appendChild(userIdCell);
+
+            var firstNameCell = document.createElement("td");
+            firstNameCell.textContent = String(result[i].FirstName);
+            row.appendChild(firstNameCell);
+
+            var lastNameCell = document.createElement("td");
+            lastNameCell.textContent = String(result[i].LastName);
+            row.appendChild(lastNameCell);
+
+            var ssnCell = document.createElement("td");
+            ssnCell.textContent = String(result[i].SSN);
+            row.appendChild(ssnCell);
+
+            var salaryCell = document.createElement("td");
+            salaryCell.textContent = String(result[i].Salary);
+            row.appendChild(salaryCell);
+
+            table.appendChild(row);
         }
-        html = html + '</tr></table>';
 
         var newdiv = document.createElement("div");
-        newdiv.innerHTML = html;
+        newdiv.appendChild(table);
         var container = document.getElementById("hiddenEmployeeRecords");
         container.appendChild(newdiv);
     });
